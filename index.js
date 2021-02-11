fd0124a9be299e0f408361423f1bf411cfcb5db9
Stephanie <stephanie.enciso13@gmail.com>
2021-02-11 15:31:58 -0600

    Second commit

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// require your server and launch it
+const server = require('./api/server.js');
+
+const PORT = 4000;
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}.`);
+});
